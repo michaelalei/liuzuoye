f7bc0962b1a8bfdf102bb1dc9c60ebaf3ab780cf
zhulei <zhulei@zhuleideMac-mini.local>
2014-10-22 21:11:35 +0800

    Initial Commit

---
+++ liuzuoye/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  liuzuoye
+//
+//  Created by zhulei on 14-10-22.
+//  Copyright (c) 2014年 zhulei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ liuzuoye/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  liuzuoye
+//
+//  Created by zhulei on 14-10-22.
+//  Copyright (c) 2014年 zhulei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
